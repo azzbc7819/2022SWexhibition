a6ac2a91e4254a861017759170fb0cc6f8b35d63
은영 <80818761+azzbc7819@users.noreply.github.com>
2022-11-21 17:31:31 +0900

    Add files via upload

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express(); // express 서버 생성
+const bodyParser = require('body-parser');
 const port = process.env.PORT || 3000;
 //let axios = require("axios");
 const sqlite3 = require("sqlite3").verbose();
@@ -13,8 +14,11 @@ const db = new sqlite3.Database('./data/mapdata.db',sqlite3.OPEN_READWRITE, (err
 });
 
 app.set('view engine', 'ejs');
-app.use(express.json())
+app.use(express.json());
 app.use(express.static(__dirname ));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+app.post('/change' );
 
 // get : 읽기(사이트) , post : 쓰기(댓글작성), put: 수정, delete: 삭제(댓글)
  // 메인 페이지
@@ -24,15 +28,13 @@ app.get('/', function(요청, 응답){
 
 });
 
-app.get('/hi', function(요청, 응답){
-    응답.sendFile(__dirname + '/example.html');
-
-
-});
 
 app.get('/searchhospital', function(요청, 응답){
     //응답.sendFile(__dirname + '/search.html');
+    var setState = false;
     var name = 요청.query.search;
+    var sOrt = 요청.query.sOrt;
+    //console.log(sOrt);
     var sql = `SELECT * from 병원 order by 병원명;`;
     if(typeof(name) != 'undefined'){
         //console.log(name);
@@ -40,29 +42,74 @@ app.get('/searchhospital', function(요청, 응답){
         //console.log(sql);
     }
 
+    if(typeof(sOrt) != 'undefined'){
+        for(var i=0;i<sOrt.length;i++){
+            var index = parseInt(sOrt[i].index);
+            var sql_sort = `UPDATE 병원 SET 정렬=`+ index + ` WHERE id=` + parseInt(i+1);
+            console.log(sql_sort);
+            db.run(sql_sort, function (err) {
+                if (err) {
+                    return console.log(err.message);
+                }
+            });
+        }
+
+        sql = `SELECT * FROM 병원 order by 정렬;`;
+    }
+
+
+
 
     db.all(sql, function(err, rows){
         rows.forEach(function (row){
             //console.log(rows[0]);
-
+            console.log(row)
             //응답.render("hospital", {model: row});
         });
-        응답.render('searchhospital',{model: rows});
+            응답.render('searchhospital',{model: rows});
+
+
 
     });
 });
+
+app.get('/searchhospital_distance', function(요청, 응답){
+    var sql = `SELECT * FROM 병원 order by 정렬;`;
+
+    db.all(sql, function(err, rows){
+        rows.forEach(function (row){
+            console.log(row);
+        });
+            응답.render('searchhospital_distance',{model: rows});
+    });
+})
+
 
 
 
 app.get('/searchpharmacy', function(요청, 응답){
     //응답.sendFile(__dirname + '/search.html');
     var name = 요청.query.search;
+    var sOrt = 요청.query.sOrt;
     var sql = `SELECT * from '약국' order by 업체명;`;
 
     if(typeof(name) != 'undefined'){
         //console.log(name);
         sql = `SELECT * FROM 약국 WHERE 업체명 LIKE "%` + name + `%" order by 업체명;`;
         //console.log(sql);
+    }
+
+    if(typeof(sOrt) != 'undefined'){
+        for(var i=0;i<sOrt.length;i++){
+            var index = parseInt(sOrt[i].index);
+            var sql_sort = `UPDATE 약국 SET 정렬=`+ index + ` WHERE id=` + parseInt(i+1);
+            console.log(sql_sort);
+            db.run(sql_sort, function (err) {
+                if (err) {
+                    return console.log(err.message);
+                }
+            });
+        }
     }
 
     db.all(sql, function(err, rows){
@@ -75,6 +122,21 @@ app.get('/searchpharmacy', function(요청, 응답){
 
     });
 });
+
+app.get('/searchpharmacy_distance', function(요청, 응답){
+    var sql = `SELECT * FROM 약국 order by 정렬;`;
+
+    db.all(sql, function(err, rows){
+        rows.forEach(function (row){
+            console.log(row);
+        });
+            응답.render('searchpharmacy_distance',{model: rows});
+    });
+})
+
+
+
+
 
 app.get('/searchpcrCenter', function(요청, 응답){
     //응답.sendFile(__dirname + '/search.html');
